d9558bed89d4bf8fb81934f8517241ffb3aac8be
xueweihan <595666367@qq.com>
2015-06-05 22:44:45 +0800

    URL构建

--- ex2_URL构建.py
+++ ex2_URL构建.py
@@ -27,7 +27,8 @@ with app.test_request_context():
     print url_for('login', next='/')
     print url_for('profile', username='Xue Wei Han')
     
-为什么不在把 URL 写死在模板中，反而要动态构建？有三个很好的理由：
+
+#为什么不在把 URL 写死在模板中，反而要动态构建？有三个很好的理由：
 
 # 1.反向解析通常比硬编码 URL 更直观。同时，更重要的是你可以只在一个地方改变 URL ,而不用到处乱找。
 # 2.URL 创建会为你处理特殊字符的转义和 Unicode 数据，不用你操心。
